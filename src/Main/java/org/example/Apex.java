636c4732ae29088fe9f04a45b566052ea54649db
Saiz1208 <sathishsekar1995@gmail.com>
2024-07-08 18:48:20 +0530

    Update Apex.java

--- src/Main/java/org/example/Apex.java
+++ src/Main/java/org/example/Apex.java
@@ -36,7 +36,7 @@ public class Apex {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-            System.out.println();
+            System.out.println("Complete");
         }
         m1();
     }
